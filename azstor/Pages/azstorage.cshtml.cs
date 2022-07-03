a957cbd6ec1440c2c5c77058c7cc4853a0d71f0c
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2022-07-03 01:48:39 +0000

    file pick and store to storage

--- azstor/Pages/azstorage.cshtml.cs
+++ azstor/Pages/azstorage.cshtml.cs
@@ -16,19 +16,64 @@ public class azstorageModel : PageModel
         _configuration = configuration;
 
     }
+    [BindProperty]
+    public string? FormData { get; set; }
+    // public string FormData2 { get; set; }
+
+    [BindProperty]
+    public IFormFile Upload { get; set; } = null!;
+    public IFormFile? Upload2 { get; set; }
 
 
-    public async Task OnGet()
+    public void OnGet()
     {
         int dog = 0;
+        dog++;
+
+    }
+
+    public async Task OnPostAsync()
+    {
+        // do something with FormData
+
+        // if (string.IsNullOrEmpty(FormData))
+        {
+            // To Do
+        };
+
+        var ms = new MemoryStream();
+        if (Upload.Length > 0)
+        {
+            await Upload.CopyToAsync(ms);
+
+        }
+
+        // if (Upload.Length > 0)        
+        //     using (var ms = new MemoryStream())        
+        //         await Upload.CopyToAsync(ms);
+
+
+        string StorageConnectionString = _configuration["AZURE_STORAGE_CONNECTION_STRING"];
+        string CocktailImageContainer = _configuration["CocktailImageContainer"];
+        string fileName = "cocktail" + Guid.NewGuid().ToString() + ".jpg";
+
+        BlobContainerClient containerClient = new BlobContainerClient(StorageConnectionString, CocktailImageContainer);
+        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+        ms.Position=0;
+        await blobClient.UploadAsync(ms);
+    }
+
+    public async Task UploadToStorage()
+    {
         string StorageConnectionString = _configuration["AZURE_STORAGE_CONNECTION_STRING"];
         string CocktailImageContainer = _configuration["CocktailImageContainer"];
 
         // Create a BlobServiceClient object which will be used to create a container client
-        BlobServiceClient blobServiceClient = new BlobServiceClient(StorageConnectionString);
+        //BlobServiceClient blobServiceClient = new BlobServiceClient(StorageConnectionString);
         // Create the container and return a container client object
         BlobContainerClient containerClient = new BlobContainerClient(StorageConnectionString, CocktailImageContainer);
-       // await containerClient.CreateAsync();
+
 
         string localPath = "./images/";
         string fileName = "cocktail" + Guid.NewGuid().ToString() + ".jpg";
@@ -36,19 +81,15 @@ public class azstorageModel : PageModel
 
         fileName = "./images/empress.jpg";
         if (System.IO.File.Exists(fileName))
-            dog++;
+        {
+            // To Do
+        };
 
         // Get a reference to a blob
-        fileName = "belvedere.jpg";
+        //fileName = "belvedere.jpg";
         BlobClient blobClient = containerClient.GetBlobClient(fileName);
 
-        
-     
-        await blobClient.UploadAsync(fileName,true);
-
-
-        dog++;
-
+        await blobClient.UploadAsync(fileName, true);
 
     }
 }
