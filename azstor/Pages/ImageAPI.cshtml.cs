e64fe1f1516265fb66b1fe8dd47aa5c8da9e53ea
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2022-07-06 20:56:57 +0000

    created API proj, set up basic calling from webapp

---
+++ azstor/Pages/ImageAPI.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NetVips;
+
+
+namespace azstor.Pages;
+
+public class ImageAPIModel : PageModel
+{
+    private HttpClient APIclient = new HttpClient();
+
+    // private readonly IConfiguration _configuration;
+
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public ImageAPIModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public string? myAPIMessage { get; set; }
+    public string apiUrl { get; set; } = "http://127.0.0.1:5136/";
+
+    [BindProperty]
+    public string? APIRoute { get; set; }
+
+    public void OnGet()
+    {
+
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        // var apiUrl = "http://127.0.0.1:5136/";
+        // apiUrl = "http://localhost:5136/";
+
+        apiUrl = apiUrl + APIRoute;                
+        // apiUrl = apiUrl + "weatherforcast";                
+
+        var request = new HttpRequestMessage(HttpMethod.Get, apiUrl);
+        var response1 = await APIclient.SendAsync(request);
+
+        switch (response1.StatusCode)
+        {
+            case System.Net.HttpStatusCode.OK:
+                myAPIMessage = await response1.Content.ReadAsStringAsync();
+                break;
+            case System.Net.HttpStatusCode.NoContent:
+                myAPIMessage = "No content";
+                break;
+            case System.Net.HttpStatusCode.NotFound:
+                myAPIMessage = "API Route not found!";
+                break;
+            case System.Net.HttpStatusCode.Forbidden:
+                myAPIMessage = "Your Access to this API route is Forbidden!";
+                break;
+            case System.Net.HttpStatusCode.Unauthorized:
+                myAPIMessage = "Your Access to this API route is Unauthorized!";
+                break;
+            default:
+                myAPIMessage = "Unhandled Error!";
+                break;
+
+        }
+
+        return Page();
+
+    }
+
+}
+
