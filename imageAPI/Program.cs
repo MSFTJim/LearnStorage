60f76cf6b784e4679f4bc736eda8eb03147c1fce
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2022-08-08 22:01:37 +0000

    1st clean up attempt

--- imageAPI/Program.cs
+++ imageAPI/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<OtherAPIFile>();
 
 var app = builder.Build();
 
@@ -23,63 +21,30 @@ else
     app.UseHttpsRedirection();
 }
 
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.MapPost("/upload",
-    async Task<IResult> (IConfiguration config, HttpRequest request) =>
-        {
-            if (!request.HasFormContentType)
-                return Results.BadRequest();
-
-            var filePath = config["Dog"];
-
-
-            var form = await request.ReadFormAsync();
-            var formFile = form.Files["file"];
-
-            if (formFile is null || formFile.Length == 0)
-                return Results.BadRequest();
-
-            await using var stream = formFile.OpenReadStream();
-
-            var reader = new StreamReader(stream);
-            var text = await reader.ReadToEndAsync();
-
-            return Results.Ok(text);
-        });
-
 var RouteHandler = new ImageHandler();
 app.MapPost("/ImageStor", RouteHandler.WriteImagetoStorage);
 
+app.MapPost("/upload", Bobo);
+
 string webRootPath = app.Environment.WebRootPath;
 
-using (var scope = app.Services.CreateScope())
-{
-    OtherAPIFile? service = scope.ServiceProvider.GetService<OtherAPIFile>();
-    // if (service is not null)
-        service.RegisterEmployeeAPIs(app);
-    
-}
 
 app.Run();
+
+IResult Bobo()
+{
+    // method code goes here
+    int dog = 0;
+    dog = Hoho();
+    return Results.Ok("Hello from Bobo");
+
+}
+
+int Hoho()
+{
+    return 1;
+}
+
 public class ImageHandler
 {
     public async Task<string> WriteImagetoStorage(IConfiguration config, HttpRequest request)
@@ -120,8 +85,4 @@ public class ImageHandler
         return 1;
     }
 } //End Class ImageHandler
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
