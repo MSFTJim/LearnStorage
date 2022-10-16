8fc734a6f58b4123750a284d3d021484947c9a57
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2022-10-16 23:29:02 +0000

    added controller api project

---
+++ C-API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace C_API.Controllers;
+
+[ApiController]
+//[Route("[controller]")]
+ [Route("Cocktails")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    // [HttpGet(Name = "WeatherForecast")]
+      [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }    
+    
+    [HttpGet("goofy")]
+     
+    public string GetGoofy()
+    {
+        return "Goofy";
+
+        }
+}
